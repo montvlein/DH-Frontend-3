0ae264b80eea7bed934f858e39317f9d23516edf
montlein <montvlein@gmail.com>
2022-08-11 18:41:49 -0400

    ej clase 6

---
+++ c6s/src/components/ClassComponent.jsx
@@ -0,0 +1,10 @@
+import "../styles/components.css"
+import React, { Component } from 'react'
+
+export default class ClassComponent extends Component {
+    render() {
+        return (
+        <li>{this.props.nombre} {this.props.estaEnLista ? "esta": "no está"} invitado a la fiesta</li>
+        )
+    }
+}
---
+++ c6s/src/components/FunctionComponent.jsx
@@ -0,0 +1,10 @@
+import "../styles/components.css"
+import React from 'react'
+
+const FunctionComponent = (props) => {
+return (
+<li>{props.nombre} traerá {props.tarea}</li>
+)
+}
+
+export default FunctionComponent
