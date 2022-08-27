bc3cf0e59f671b840a4ab068fa9c8ef4850722ef
montvlein <montvlein@gmail.com>
2022-08-27 13:05:53 -0300

    se importo 'useState' y se useo para crear e inicializar en '0' la variable 'cant'. Se paso la variable como prop del componente 'Cabecera'

--- entregable-frontend-3-junio22/src/App.js
+++ entregable-frontend-3-junio22/src/App.js
@@ -5,14 +5,16 @@
 // MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
 // PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
 
+import { useState } from 'react'
 import Cabecera from './components/Cabecera'
 import Listado from "./components/Listado"
 
 export default function App() {
+  const [cant, setCantidad] = useState(0)
 
   return (
     <div className="App">
-      <Cabecera cantidad={0}/>
+      <Cabecera cantidad={cant}/>
       <Listado />
     </div>
   );
